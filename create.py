9a687d70a81a143ee132541a6ce64464697d54d2
Vitaliy Martynov <votolm@gmail.com>
2019-02-21 23:02:29 +0300

    update create script: Workflow as submodule

--- create.py
+++ create.py
@@ -5,6 +5,8 @@ import ruamel.yaml
 import os
 import uuid
 import shutil
+import subprocess
+
 
 parser = argparse.ArgumentParser(description='Creation parameters')
 parser.add_argument('-n','--name', required=True, help='name of new project')
@@ -17,6 +19,7 @@ if os.path.isdir(args.name):
 yaml = ruamel.yaml.YAML()
 yaml.indent(mapping=2, sequence=4, offset=2)
 work_dir = os.path.dirname(os.path.realpath(__file__))
+current_dir = os.getcwd()
 
 #creating folder structure
 os.mkdir(args.name)
@@ -24,25 +27,49 @@ os.mkdir(args.name + "/src")
 os.mkdir(args.name + "/workkDir")
 os.mkdir(args.name + "/test")
 
+try:
+    #base description yaml
 
-#base description yaml
+    yaml_stream = open(work_dir + "/resources/description.yaml", "r")
+    description = yaml.load(yaml_stream)
+    yaml_stream.close()
 
-yaml_stream = open(work_dir + "/resources/description.yaml", "r")
-description = yaml.load(yaml_stream)
-yaml_stream.close()
+    description["general"]["uuid"] = str(uuid.uuid4())
+    description["general"]["name"] = args.name
 
-description["general"]["uuid"] = str(uuid.uuid4())
-description["general"]["name"] = args.name
+    with open(args.name + "/description.yaml", "w") as outfile:
+        yaml.dump(description, outfile)
 
-with open(args.name + "/description.yaml", "w") as outfile:
-    yaml.dump(description, outfile)
+    #generate base CMakeLists.txt
+    with open(args.name + "/CMakeLists.txt", 'w') as outfile:
+        outfile.write("cmake_minimum_required(VERSION 3.0)\n")
+        outfile.write("project(CalcProj)\n\n")
+        outfile.write("add_subdirectory(src)\n")
 
-#generate base CMakeLists.txt
-with open(args.name + "/CMakeLists.txt", 'w') as outfile:
-    outfile.write("cmake_minimum_required(VERSION 3.0)")
-    outfile.write("project(CalcProj)\n")
-    outfile.write("add_subdirectory(src)")
+    #generate base main file
+    with open(args.name + "/src/main.cpp", 'w') as outfile:
+        outfile.write("#include \"schema.h\"\n\n")
+        outfile.write("int main(int argc, char **argv)\n{\n")
+        outfile.write("    return 0;\n}\n")
 
-#copying files
-shutil.copyfile(work_dir + "/resources/sampleProject/CMakeLists.txt", args.name + '/src/CMakeLists.txt')
-shutil.copyfile(work_dir + "/resources/sampleProject/Makefile", args.name + '/Makefile')
+    #copying files
+    shutil.copyfile(work_dir + "/resources/sampleProject/CMakeLists.txt", args.name + '/src/CMakeLists.txt')
+    shutil.copyfile(work_dir + "/resources/sampleProject/Makefile", args.name + '/Makefile')
+
+    #some things with git
+    current_dir = os.getcwd()
+    os.chdir(current_dir + "/" + args.name)
+    subprocess.run(["git init"], shell=True)
+    subprocess.run(["git submodule add git@github.com:votol/Workflow.git Workflow"], shell=True)
+    subprocess.run(["git add ."], shell=True)
+    subprocess.run(["git commit -m \"initial commit\""], shell=True)
+    os.chdir(current_dir)
+except subprocess.CalledProcessError as e:
+    print("Failed to create project: " + e.output)
+    os.chdir(current_dir)
+    shutil.rmtree(args.name)
+except Exception as e:
+    print("Failed to create project: " + str(e))
+    os.chdir(current_dir)
+    shutil.rmtree(args.name)
+
