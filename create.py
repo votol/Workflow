ed948978e223078f07713566df8f7d46ec424d4e
Vitaliy Martynov <votolm@gmail.com>
2019-03-04 00:41:09 +0300

    auto creation of github repo

--- create.py
+++ create.py
@@ -6,11 +6,14 @@ import os
 import uuid
 import shutil
 import subprocess
+import urllib.request as urllib
+import base64, json
 import scripts.ccs.general as ccs
 
 
 parser = argparse.ArgumentParser(description='Creation parameters')
 parser.add_argument('-n','--name', required=True, help='name of new project')
+parser.add_argument('-r', action='store_true', help='defines if the project should be added to github')
 args = parser.parse_args()
 
 #prepairing
@@ -21,13 +24,43 @@ if not ccs.checkName(args.name):
 if os.path.isdir(args.name):
     print("Sorry but project with given name already exists.")
     exit(-1)
+if args.r:
+    #getting token
+    username = "votol"
+    token = os.environ["GH_TOKEN"]
+    url = "https://api.github.com/user/repos"
+    #checking if the project already exists
+    request = urllib.Request(url)
+    b64auth = base64.standard_b64encode(bytes("%s:%s" % (username, token), 'UTF-8'))
+    request.add_header("Authorization", "Basic %s" %  b64auth.decode('UTF-8'))
+    result = json.loads(urllib.urlopen(request).read().decode())
+    for element in result:
+        if "votol/" not in element["full_name"]:
+            continue
+        project_name = element["full_name"].replace("votol/", "")
+        if project_name == args.name:
+            print("Sorry, project with this name exists on github")
+            exit(-1)
+
 yaml = ruamel.yaml.YAML()
 yaml.indent(mapping=2, sequence=4, offset=2)
 work_dir = os.path.dirname(os.path.realpath(__file__))
 current_dir = os.getcwd()
 
-#creating folder structure
 os.mkdir(args.name)
+if args.r:
+    url = "https://api.github.com/user/repos"
+    param_map = {"name" : args.name, "has_wiki": "false"}
+    param_json = json.dumps(param_map).encode('utf8')
+    request = urllib.Request(url, data=param_json, headers={'content-type': 'application/json'})
+    request.add_header("Authorization", "Basic %s" % b64auth.decode('UTF-8'))
+    response = urllib.urlopen(request)
+    subprocess.run(["git clone git@github.com:votol/" + args.name + ".git"], shell=True)
+
+else:
+    os.mkdir(args.name)
+
+#creating folder structure
 os.mkdir(args.name + "/src")
 os.mkdir(args.name + "/workDir")
 os.mkdir(args.name + "/test")
@@ -82,10 +115,15 @@ try:
     #some things with git
     current_dir = os.getcwd()
     os.chdir(current_dir + "/" + args.name)
-    subprocess.run(["git init"], shell=True)
+    if not args.r:
+        subprocess.run(["git init"], shell=True)
+
     subprocess.run(["git submodule add git@github.com:votol/Workflow.git Workflow"], shell=True)
     subprocess.run(["git add ."], shell=True)
     subprocess.run(["git commit -m \"initial commit\""], shell=True)
+
+    if args.r:
+        subprocess.run(["git push -u origin master"], shell=True)
 except subprocess.CalledProcessError as e:
     print("Failed to create project: " + e.output)
     os.chdir(current_dir)
