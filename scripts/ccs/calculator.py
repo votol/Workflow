abb8fa39d7951e82c93293fe1c74be761acceeda
Vitaliy Martynov <votolm@gmail.com>
2019-06-05 00:03:53 +0300

    first version of calculator function

--- scripts/ccs/calculator.py
+++ scripts/ccs/calculator.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+import os
+import shutil
+import uuid
+import subprocess
+import threading
+import queue
+import ruamel.yaml
 import ccs.projects as pr
 
 def buildTask(revision, data_list, property_map = None):
@@ -56,6 +63,9 @@ def buildTask(revision, data_list, property_map = None):
         tmp_result = builders[0].build()
         if tmp_result == None:
             break
+        if revision.contains(tmp_result):
+            continue
+        
         adding_element = {}
         adding_element["config"] = {}
         adding_element["config"]["parameters"] = tmp_result
@@ -67,9 +77,65 @@ def buildTask(revision, data_list, property_map = None):
     return result
 
 
-class calculator:
-    def __init__(self, node_list, revision):
-        return
+class Calculator:
+    def __init__(self, node_list):
+        self.nodes = node_list
     
-    def calculate(self):
-        return
+    def calculate(self, items):
+        def NodeFunc(node_id):
+            while True:
+                try:
+                    work_item = input_queue.get_nowait()
+                except queue.Empty:
+                    output_queue.put(None)
+                    break 
+                work_dir = "/tmp/ccs/" + str(uuid.uuid4())
+                os.mkdir(work_dir)
+                os.mkdir(work_dir + "/tmp")
+                work_item["config"]["properties"]["cuda_device"] = node_id
+                work_item["config"]["properties"]["output_path"] = work_dir
+                work_item["config"]["properties"]["tmp_path"] = work_dir + "/tmp"
+                yaml = ruamel.yaml.YAML()
+                with open(work_dir + "/config.yaml", "w") as outfile:
+                    yaml.dump(work_item["config"], outfile)
+                
+                proc = subprocess.Popen([work_item["revision"].binary(), work_dir + "/config.yaml"])
+                result = proc.wait()
+                answer = {}
+                answer["directory"] = work_dir
+                answer["revision"] = work_item["revision"]
+                answer["status"] = result 
+                output_queue.put(answer)
+        
+        if not os.path.exists("/tmp/ccs"):
+            os.makedirs("/tmp/ccs")
+        
+        input_queue = queue.SimpleQueue()
+        output_queue = queue.SimpleQueue()
+        
+        for item in items:
+            input_queue.put(item)
+        
+        threads = []
+        for item in self.nodes:
+            t = threading.Thread(target=NodeFunc, args = (item,))
+            t.start()
+            threads.append(t)
+        
+        finished_nodes = 0
+        while True:
+            res_element = output_queue.get()
+            if res_element is None:
+                finished_nodes += 1
+                if finished_nodes == len(self.nodes):
+                    break
+                continue
+            if res_element["status"] == 0:
+                res_element["revision"].add(res_element["directory"])
+                
+            shutil.rmtree(res_element["directory"])
+            
+            
+        for item in threads:
+            item.join()
+            
